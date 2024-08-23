de58b95f492d801d18724134cda6de6dd32d8b68
nenoeldeeb <69523673+Nenoeldeeb@users.noreply.github.com>
2024-08-24 01:19:07 +0300

    Migrating from using Gradle with Grovy to use Gradle with Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,13 +9,9 @@ buildscript {
 			"1.2.0"
 		)
 	}
-	repositories {
-		google()
-		mavenCentral()
-	}
-	dependencies {
-		classpath("com.android.tools.build:gradle:7.3.0")
-		classpath("org.jetbrains.dokka:dokka-gradle-plugin:${rootProject.extra["kotlin_version"]}")
-		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${rootProject.extra["kotlin_version"]}")
-	}
+}
+plugins {
+	id("com.android.library") version "7.3.0" apply false
+	id("org.jetbrains.kotlin.android") version "1.7.0" apply false
+	id("org.jetbrains.dokka") version "1.7.0" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,19 @@ dependencyResolutionManagement {
 		mavenCentral()
 	}
 }
+pluginManagement {
+	repositories {
+		google {
+			content {
+				includeGroupByRegex("com\\.android.*")
+				includeGroupByRegex("com\\.google.*")
+				includeGroupByRegex("androidx.*")
+			}
+		}
+		mavenCentral()
+		gradlePluginPortal()
+	}
+}
 rootProject.name = "Form Builder"
 include(":example")
 include(":form-builder")
